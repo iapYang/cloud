e6111876555e08b36e5860df0ceb51443a7bba18
DESKTOP-8IQMI96\Creasy <yklorse@gmail.com>
2017-08-03 16:41:13 +0800

    all finished

--- dev/script/index.js
+++ dev/script/index.js
@@ -3,6 +3,8 @@ import $ from 'jquery';
 import '../style/style.scss';
 
 const $menu = $('.menu');
+const $section5 = $('.home-section-5');
+const $window = $(window);
 
 $('.menu-icon').on('click', () => {
     if ($menu.hasClass('active')) {
@@ -10,4 +12,11 @@ $('.menu-icon').on('click', () => {
     } else {
         $menu.addClass('active');
     }
+});
+
+$window.on('scroll', e => {
+    if (window.pageYOffset < $section5.position().top - $window.height()) return;
+    if ($section5.hasClass('fadeIn')) return;
+
+    $section5.addClass('fadeIn');
 });
